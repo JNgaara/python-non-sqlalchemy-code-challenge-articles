b8f3c40b97b2ecebfab2ef1ddc0ee93b7b33c88d
+ <joshngaara@gmail.com>
2025-03-03 20:40:54 +0200

    attribute and value error check

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,24 +1,50 @@
 class Article:
+    articles = []
     def __init__(self, author, magazine, title):
-        if hasattr(self, 'author'):
-            raise AttributeError("Cannot modify the author after initialisation")
-        
+        if not isinstance(author, Author):
+            raise TypeError("Author must be an instance of Author")
+        if not isinstance(magazine, Magazine):
+            raise TypeError("Magazine must be an instance of Magazine")
+        if not isinstance(title, str):
+            raise ValueError("Title must be an non-empty string between 5-50 characters")
         self.author = author(author)
         self.magazine = magazine(magazine)
         self.title = str(title)
+        Article.articles.append(self)
 
     def articletitle(self):
-        return print(f" The article title is {self.title} ") 
+        return {self.title} 
+    
+    def author(self):
+        return self.author
+    
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError("Author must be an instance of Author")
+        self.author = value
+    def magazine(self):
+        return self.magazine
+    
+    def magazine(self, value):
+         if not isinstance(value, Magazine):
+            raise TypeError("Magazine must be an instance of Magazine")
+         self.magazine = value
         
 class Author:
     def __init__(self, name):
+        
+        if hasattr(self, 'author'):
+            raise AttributeError("Cannot modify the author after initialisation")
+        if not isinstance(author, str) or len(author)==0:
+            raise ValueError("The author name must be  a non-empty string")
+        
         self.name = str(name)
     
     def fullname(self):
         return print(f" The author is {self.name} ") 
 
     def articles(self):
-        pass
+        return [article for article in Article.articles if article.magazine == self]
 
     def magazines(self):
         pass
