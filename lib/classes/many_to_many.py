03ce100c79ee0b71c0ccd216a98262c784a96b87
+ <joshngaara@gmail.com>
2025-03-03 16:26:47 +0200

    article return title

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,14 +2,17 @@ class Article:
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self.title = str(title)
+
+    def articletitle(self):
+        return f" The article title is {self.title} "
         
 class Author:
     def __init__(self, name):
         self.name = str(name)
     
     def fullname(self):
-        return f""
+        return f" The author is {self.name} "
 
     def articles(self):
         pass
@@ -27,6 +30,13 @@ class Magazine:
     def __init__(self, name, category):
         self.name = str(name)
         self.category = str(category)
+    
+    def magazinename(self):
+        return f" The magazine title is {self.name} "
+    
+    def magazinecategory(self):
+        return f" The magazine category is {self.category} "
+
 
     def articles(self):
         pass
